b8a35b59083dbb170648f3f2474ad7a1e1ee3bba
olbrine <oxt88@miami.edu>
2020-03-31 15:33:03 -0400

    save

--- class_examples/jquery-intro/main.js
+++ class_examples/jquery-intro/main.js
@@ -1,8 +1,8 @@
-$(document).ready(funtion(){
+$(document).ready(function(){
 // all js goes here
   console.log("we loaded");
 
 $("title").css("color", "#bada55");
-$("title").css("color", "125px");
+$("title").css("font-size", "125px");
 $("title").html("More Information #1");
 })
