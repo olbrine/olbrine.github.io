6549e21959a1f5b750d5384f9a0fdbe730e2dae6
olbrine <oxt88@miami.edu>
2020-04-01 04:21:44 -0400

    save

--- class_examples/jquery-intro/main.js
+++ class_examples/jquery-intro/main.js
@@ -2,8 +2,11 @@ $(document).ready(function() {
   // all js goes here
   console.log("we loaded");
 
-  $("#title").css("color", "orange");
-  $("#title").css("font-size", "125px");
-  $("#title").html("How to Thrift");
-
+  $("#title").css("color", "MediumSpringGreen");
+  $("#title").css("font-size", "95px");
+  $("#title").html("How to Make a Tie-Dye Shirt");
+  $("#heading1").css("color", "gold");
+  $("#heading2").css("color", "Turquoise");
+  $("#heading3").css("color", "Magenta");
+  $("#heading4").css("color", "DarkOrange");
 })
