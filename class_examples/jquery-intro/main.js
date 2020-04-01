878c05f3ed1347a4e315274de3aa4ed2d616aef8
olbrine <oxt88@miami.edu>
2020-04-01 13:37:58 -0400

    save

--- class_examples/jquery-intro/main.js
+++ class_examples/jquery-intro/main.js
@@ -2,11 +2,35 @@ $(document).ready(function() {
   // all js goes here
   console.log("we loaded");
 
-  $("#title").css("color", "MediumSpringGreen");
+  $("#button1").click(function() {
+    // $("#title").css("color", "#f9423a");
+    // $("#title").css("font-size, 500px")
+    $("#title").addClass("big-text");
+  })
+
+  $("#button2").click(function() {
+    $("#title").removeClass("big-text");
+  })
+
+  $("p").mouseover(function() {
+    $("p").css("font-size", "21px");
+
+  })
+
+  $("p").mouseleave(function() {
+    $("p").css("background-color", "pink");
+    $("p").css("font-size", "16px")
+
+  })
+  // $("#title").css("color", "MediumSpringGreen");
   $("#title").css("font-size", "95px");
   $("#title").html("How to Make a Tie-Dye Shirt");
   $("#heading1").css("color", "gold");
   $("#heading2").css("color", "Turquoise");
   $("#heading3").css("color", "Magenta");
   $("#heading4").css("color", "DarkOrange");
+
+
+
+
 })
