7bb659a8b4b04b59758ff8c5ec8f2cb2f20e4a0a
olbrine <oxt88@miami.edu>
2020-04-13 13:29:23 -0400

    save

--- hw/jquery-intro/main.js
+++ hw/jquery-intro/main.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
   // all js goes here
   console.log("we loaded");
+
+  $("p").mouseover(function() {
+    $("p").css("font-size", "37px");
+    $("p").css("background-color", "white");
+
+  })
+  $("p").mouseleave(function() {
+    $("p").css("font-size", "30px")
+    $("p").removeClass("background-color");
+  })
+
 })
