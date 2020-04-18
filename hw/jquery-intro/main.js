e8c7c136075d6cd66ecef5981781983a32be74da
olbrine <oxt88@miami.edu>
2020-04-17 21:56:00 -0400

    save

--- hw/jquery-intro/main.js
+++ hw/jquery-intro/main.js
@@ -2,16 +2,44 @@ $(document).ready(function() {
   // all js goes here
   console.log("we loaded");
 
+  let titleClicked = false;
+  $("#heading").click(function() {
+    if (titleClicked == false) {
+
+      $("heading").html("Best Places to Go");
+
+      titleClicked = true;
+    } else if (titleClicked == true) {
+
+      $("#heading").html("Olbrine's Favorite Places to Go");
+
+      titleClicked = false;
+    }
+  })
+  $("#hide").click(function() {
+    $("h4").hide();
+  });
+
+  $("#show").click(function() {
+    $("h4").show();
+  });
   $("p").mouseover(function() {
     $("p").css("font-size", "45px");
     $("p").css("background-color", "purple");
   })
+
   $("p").mouseleave(function() {
-    $("p").css("font-size", "40px")
+    $("p").css("font-size", "40px");
     $("p").css("background-color", "transparent");
   })
 
-  $("#NOMA").mouseover(function() {
-    $("#NOMA").css("backgroud-color", "#4d3219");
+  $("h4").mouseover(function() {
+    $("h4").css("background-color", "white");
+    $("h4").css("font-size", "45px");
+  })
+
+  $("h4").mouseleave(function() {
+    $("h4").css("background-color", "transparent");
+    $("h4").css("font-size", "40px");
   })
 })
