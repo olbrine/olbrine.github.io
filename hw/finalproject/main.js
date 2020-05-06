dcb392842b58c4c804449c3ddd092db452d33a6d
olbrine <oxt88@miami.edu>
2020-05-06 12:42:52 -0400

    save

--- hw/finalproject/main.js
+++ hw/finalproject/main.js
@@ -2,6 +2,46 @@ $(document).ready(function() {
   // all js goes here
   console.log("we loaded");
 
+  $("#shoepic1").mouseover(function() {
+    $("#shoepic1").attr("src", "Shoes1.png");
+  });
 
+  $("#shoepic1").mouseleave(function() {
+    $("#shoepic1").attr("src", "Shoe1.png");
+  });
+  $("#shoepic2").mouseover(function() {
+    $("#shoepic2").attr("src", "Shoes2.png");
+  });
 
+  $("#shoepic2").mouseleave(function() {
+    $("#shoepic2").attr("src", "Shoe2.png");
+  });
+  $("#shoepic3").mouseover(function() {
+    $("#shoepic3").attr("src", "Shoes3.png");
+  });
+
+  $("#shoepic3").mouseleave(function() {
+    $("#shoepic3").attr("src", "Shoe3.png");
+  });
+  $("#shoepic4").mouseover(function() {
+    $("#shoepic4").attr("src", "Shoes4.png");
+  });
+
+  $("#shoepic4").mouseleave(function() {
+    $("#shoepic4").attr("src", "Shoe4.png");
+  });
+  $("#shoepic5").mouseover(function() {
+    $("#shoepic5").attr("src", "Shoes5.png");
+  });
+
+  $("#shoepic5").mouseleave(function() {
+    $("#shoepic5").attr("src", "Shoe5.png");
+  });
+  $("#shoepic6").mouseover(function() {
+    $("#shoepic6").attr("src", "Shoes6.png");
+  });
+
+  $("#shoepic6").mouseleave(function() {
+    $("#shoepic6").attr("src", "Shoe6.png");
+  });
 })
